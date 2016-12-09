cef62e25e1047bdfca1a55e681c7b98751c74e02
Dirk Rüdiger <dirk@niebegeg.net>
2016-12-09 08:23:49 +0100

    More robust update_assembled_configs.sh

--- update_assembled_configs.sh
+++ update_assembled_configs.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 command -v assemble-config >/dev/null 2>&1 || ( echo "Missing command 'assemble-config', exiting." ; exit 0 )
 
 for ddir in $(find $(dirname $0) -name *.d -type d)
 do
-  assemble-config ${ddir/.d$/}
+  assemble-config ${ddir%.d}
 done
