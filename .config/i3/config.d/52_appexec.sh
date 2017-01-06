277638c2d859027d778d9118a4305317351fbe09
Dirk Rüdiger <dirk@niebegeg.net>
2017-01-06 08:18:12 +0100

    Saved workspace layout for workspace 1, autostart mail and chat.

--- .config/i3/config.d/52_appexec.sh
+++ .config/i3/config.d/52_appexec.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo exec --no-startup-id "i3-msg 'workspace \$workspace1; append_layout $HOME/.config/i3/workspace-1.json'"
+
 if [ -e  /usr/bin/gnome-settings-daemon ]; then
   echo "exec /usr/bin/gnome-settings-daemon"
 elif [ -e  /usr/libexec/gnome-settings-daemon ]; then
@@ -12,7 +14,7 @@ fi
 
 echo "exec_always --no-startup-id feh --bg-scale /home/dirk/.local/share/bg_kosmonaut.jpg"
 
-for c in firefox thunderbird xchat-gnome owncloud nm-applet
+for c in firefox thunderbird owncloud nm-applet
 do
   if [ -e "/usr/bin/${c}" ]; then
     echo "exec $c"
@@ -26,3 +28,7 @@ do
     break
   fi
 done
+
+echo "exec i3-sensible-terminal -name term -title term"
+echo "exec i3-sensible-terminal -name mail -title mail -e mutt"
+echo "exec i3-sensible-terminal -name chat -title chat -e irssi"
