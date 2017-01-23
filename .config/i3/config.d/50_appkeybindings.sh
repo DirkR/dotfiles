f6f5a75a854715ff216040a4eca9e88daec32491
Dirk Rüdiger <dirk@niebegeg.net>
2017-01-23 13:20:16 +0100

    Repaired i3exit and system menu.

--- .config/i3/config.d/50_appkeybindings.sh
+++ .config/i3/config.d/50_appkeybindings.sh
@@ -1,12 +1,21 @@
+#!/bin/sh
+
 # screen locker (first move to "safe" workspace without any chat app)
-bindsym $mod+Control+l exec i3-msg workspace 1 && i3lock -c 111111 -d
+echo 'bindsym $mod+Control+l exec i3-msg workspace 1 && i3lock -c 111111 -d'
 
 # start a terminal
-bindsym $mod+Return exec x-terminal-emulator
+echo 'bindsym $mod+Return exec x-terminal-emulator'
 
-# start dmenu (a program launcher)
-#bindsym $mod+d exec i3-dmenu-desktop
-bindsym $mod+d exec rofi -show run
-bindsym $mod+shift+t exec thunar
+if [ -e /usr/bin/rofi ]; then
+  echo 'bindsym $mod+d exec rofi -show run'
+else
+  echo 'bindsym $mod+d exec i3-dmenu-desktop'
+fi
+echo 'bindsym $mod+shift+t exec thunar'
+echo 'bindsym $mod+Shift+f exec firefox'
 
-bindsym $mod+Shift+f exec firefox
+if [ -e /usr/bin/scrot ]; then
+  echo 'bindsym --release Shift+Print exec scrot -s "screenshot_%Y-%m-%d_$wx$h.png" -e "mv $f ~/Pictures/"'
+elif [ -e /usr/bin/gnome-screenshot ]; then
+  echo 'bindsym $mod+Shift+s exec "gnome-screenshot -a"'
+fi
