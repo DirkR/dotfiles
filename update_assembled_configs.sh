ac9ea7a52831c6b49a78672f13631d9b028eff88
Dirk Rüdiger <>
2018-11-08 11:27:28 +0100

    Repaired execution of assemble-config in 'dfm in'.

--- update_assembled_configs.sh
+++ update_assembled_configs.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
-command -v assemble-config >/dev/null 2>&1 || ( echo "Missing command 'assemble-config', exiting." ; exit 0 )
-
+HERE=$(realpath $(dirname $0))
 for ddir in $(find $(dirname $0) -name *.d -type d)
 do
-  assemble-config ${ddir%.d}
+  $HERE/.local/bin/assemble-config ${ddir%.d}
 done
