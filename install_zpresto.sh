fa9e82af5c6040c3caa4689e2120b75173021e1e
Dirk Rüdiger <>
2019-10-03 07:53:18 +0200

    Update zpresto instead of installing, if it's allready installed

--- install_zpresto.sh
+++ install_zpresto.sh
@@ -2,4 +2,6 @@
 ZPRESTO_DIR="${ZDOTDIR:-$HOME}/.zprezto"
 if [ ! -d "$ZPRESTO_DIR" ]; then
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "$ZPRESTO_DIR"
+else
+  ( cd  "$ZPRESTO_DIR" ; git pull ; git submodule update --recursive )
 fi
