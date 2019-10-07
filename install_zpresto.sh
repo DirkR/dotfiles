e93c5c8c0b9ea59c13f4048cb714d543e3db1887
Dirk Rüdiger <>
2019-10-07 07:12:30 +0200

    Better update for zpresto submodules

--- install_zpresto.sh
+++ install_zpresto.sh
@@ -3,5 +3,5 @@ ZPRESTO_DIR="${ZDOTDIR:-$HOME}/.zprezto"
 if [ ! -d "$ZPRESTO_DIR" ]; then
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "$ZPRESTO_DIR"
 else
-  ( cd  "$ZPRESTO_DIR" ; git pull ; git submodule update --recursive )
+  ( cd  "$ZPRESTO_DIR" ; git pull ; git submodule init ; git submodule update --recursive --init )
 fi
