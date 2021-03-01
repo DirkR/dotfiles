249c4dba5a9e30cdbc638cf5f0c290fe302f99c1
Dirk Rüdiger <ruediger@f2e.de>
2021-03-01 08:17:49 +0100

    Removed typo

--- .zsh_custom/php74.zsh
+++ .zsh_custom/php74.zsh
@@ -1,7 +1,7 @@
 # Setup a xcommand to run scripts with php7.4, even if the default php is v8.x
 # I use it on my macOS computers 
 #
-if [ -d /usr/local/Cellar/]
+if [ -d /usr/local/Cellar/ ]
 then
   PHP74=$(which /usr/local/Cellar/php@7.4/*/bin/php)
 
