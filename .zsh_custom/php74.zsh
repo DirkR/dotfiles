854505aa18919ccdef8f22f1a1b17a9c01d8f68a
Dirk Rüdiger <ruediger@f2e.de>
2021-03-01 08:16:11 +0100

    set alias for php74 only on macOS hosts

--- .zsh_custom/php74.zsh
+++ .zsh_custom/php74.zsh
@@ -1,9 +1,11 @@
 # Setup a xcommand to run scripts with php7.4, even if the default php is v8.x
 # I use it on my macOS computers 
 #
+if [ -d /usr/local/Cellar/]
+then
+  PHP74=$(which /usr/local/Cellar/php@7.4/*/bin/php)
 
-PHP74=$(which /usr/local/Cellar/php@7.4/*/bin/php)
-
-alias php74="$PHP74"
-alias xphp74="XDEBUG_CONFIG=idekey=PHPSTORM $PHP74"
+  alias php74="$PHP74"
+  alias xphp74="XDEBUG_CONFIG=idekey=PHPSTORM $PHP74"
+fi
 
